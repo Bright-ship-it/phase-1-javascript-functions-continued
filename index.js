c3bf240bac1743211096eeb1c0261b874bcbf8a2
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-06-09 07:54:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+saturdayFun();
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`;
+    } else {
+        return 'This Monday, I will go to the office.';
+    }
+}
+
+let wrapAdjective = function(sign = "*") {
+    return function(shelby = "special") {
+        return `You are ${sign}${shelby}${sign}!`;
+    }
+}
